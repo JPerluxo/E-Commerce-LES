8504292dc85973b5ad85c1d1bcd82fe455010d0c
Jefferson Perluxo Clemente <jefferson.clemente@muralis.com.br>
2024-08-28 10:19:14 -0300

    [FRONTEND] Inserção de ícone de indicadores no menu do admin

--- les-frontend/frontend_les/src/concepts/ecommerce_admin/components/organisms/MenuBox/index.jsx
+++ les-frontend/frontend_les/src/concepts/ecommerce_admin/components/organisms/MenuBox/index.jsx
@@ -5,6 +5,7 @@ import MenuItem from '../../molecules/MenuItem';
 import GerenciarClientePng from '../../../../../images/home-admin/gerenciar-cliente.png';
 import GerenciarProdutoPng from '../../../../../images/home-admin/gerenciar-produto.png';
 import GerenciarCupomPng from '../../../../../images/home-admin/gerenciar-cupom.png';
+import IndicadoresPng from '../../../../../images/home-admin/indicadores.png';
 
 const MenuBox = () => {
   return (
@@ -13,6 +14,7 @@ const MenuBox = () => {
         <MenuItem link="./manageUser" image={GerenciarClientePng} text="Gerenciar Cliente"/>
         <MenuItem link="./productForm" image={GerenciarProdutoPng} text="Gerenciar Produto"/>
         <MenuItem link="./coupomForm" image={GerenciarCupomPng} text="Gerenciar Cupom"/>
+        <MenuItem link="#" image={IndicadoresPng} text="Indicadores"/>
       </ul>
     </div>
     
