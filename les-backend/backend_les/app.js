803745628e5c399a77574a2e2148231ea76b36fb
Jefferson Perluxo Clemente <jefferson.clemente@muralis.com.br>
2024-08-28 10:45:59 -0300

    [BACKEND] Instalação da bliblioteca do cors no projeto

--- les-backend/backend_les/app.js
+++ les-backend/backend_les/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.get('/', (req, res) => res.send('Server OK!'));
 
 // Controllers
