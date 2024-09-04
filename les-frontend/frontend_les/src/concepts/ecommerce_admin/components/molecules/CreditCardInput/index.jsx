f15eb3eb9444062370f0b706a01b31c02125d762
Jefferson Perluxo Clemente <jefferson.clemente@muralis.com.br>
2024-09-04 08:27:04 -0300

    [FRONTEND] Correções no formulário de usuários

--- les-frontend/frontend_les/src/concepts/ecommerce_admin/components/molecules/CreditCardInput/index.jsx
+++ les-frontend/frontend_les/src/concepts/ecommerce_admin/components/molecules/CreditCardInput/index.jsx
@@ -9,13 +9,13 @@ import Button from 'react-bootstrap/Button';
 
 const CreditCardInput = ({ id, isActiveChecked, onChange, cardName, cardNum, cardDueDate, flag, cardCvv, removeFunction }) => {
   const [selectedFlag, setSelectedFlag] = useState(flag || '');
-  const flags = [{id: 1, name: "VISA"}, {id: 2, name: "MasterCard"}, {id: 3, name: "Elo"}, {id: 3, name: "American Express"}];
+  const flags = [{id: 1, name: "VISA"}, {id: 2, name: "MasterCard"}, {id: 3, name: "Elo"}, {id: 4, name: "American Express"}];
 
   return (
     <div className={`mb-3 ${styles.creditCardInputDiv}`}>
       <Form.Check inline label="Ativo" type="checkbox" defaultChecked={isActiveChecked} id={`isActiveCheckbox_${id}`} onChange={(e) => onChange('isActive', e.target.checked)}/>
       <FloatingLabel label="Nome no Cartão" className="mb-2">
-        <Form.Control type="text" defaultValue={cardName} id={`isActiveCheckbox_${id}`} onChange={(e) => onChange('name', e.target.value)}/>
+        <Form.Control type="text" defaultValue={cardName} onChange={(e) => onChange('name', e.target.value)}/>
       </FloatingLabel>
       <Row className="g-2">
         <Col md>
