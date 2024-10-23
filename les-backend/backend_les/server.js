b28ff19f939c77514ceaefcc3646f1adc6ba9aeb
Jefferson Perluxo Clemente <jefferson.clemente@muralis.com.br>
2024-10-22 21:09:57 -0300

    [BACKEND] Ajuste na mensagem de compilação do servidor

--- les-backend/backend_les/server.js
+++ les-backend/backend_les/server.js
@@ -4,6 +4,8 @@ const port = 3001;
 
 db.sync().then(() => {
     app.listen(port, () => {
+        console.clear();
+        console.log('\x1b[32m%s\x1b[0m', 'Compiled successfully!', '\n');
         console.log(`Server is running on http://localhost:${port}`);
     });
 });
