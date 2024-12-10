5a3423e6bac63e4dca94956d797a833184058e63
Jefferson Perluxo Clemente <jperluxo@gmail.com>
2024-12-10 18:50:53 -0300

    [FRONTEND] Correção de bugs

--- les-frontend/frontend_les/src/concepts/ecommerce_admin/components/organisms/MenuBox/index.jsx
+++ les-frontend/frontend_les/src/concepts/ecommerce_admin/components/organisms/MenuBox/index.jsx
@@ -4,7 +4,7 @@ import styles from './index.module.css';
 import MenuItem from '../../molecules/MenuItem';
 import GerenciarClientePng from '../../../../../images/home-admin/gerenciar-cliente.png';
 import GerenciarPedidoPng from '../../../../../images/home-admin/gerenciar-pedido.png';
-import IndicadoresPng from '../../../../../images/home-admin/indicadores.png';
+// import IndicadoresPng from '../../../../../images/home-admin/indicadores.png';
 
 const MenuBox = () => {
   return (
@@ -12,7 +12,7 @@ const MenuBox = () => {
       <ul className={styles.menuBox}>
         <MenuItem link="./manageUser" image={GerenciarClientePng} text="Gerenciar Cliente"/>
         <MenuItem link="./managePurchase" image={GerenciarPedidoPng} text="Gerenciar Pedido"/>
-        <MenuItem link="#" image={IndicadoresPng} text="Indicadores"/>
+        {/* <MenuItem link="#" image={IndicadoresPng} text="Indicadores"/> */}
       </ul>
     </div>
     
